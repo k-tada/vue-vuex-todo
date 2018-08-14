2ff74c6f63495096ef65d0d1c9bb10e1287e0e08
k-tada <white.rv0925@gmail.com>
2018-08-14 18:05:47 +0900

    fix checkbox design

--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -51,14 +51,23 @@ input.toggle {
   align-items: center;
   width: 40px;
   height: 40px;
-  /* appearance: none; */
+  appearance: none;
   border: none;
 }
 
-/* input:after { */
-/*   content: url(data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" …="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>); */
-/* } */
-/*  */
+input.toggle:after {
+  content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>');
+}
+
+input.toggle:checked:after {
+  content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>');
+}
+
+input.toggle:checked + .text {
+  Color: #d9d9d9;
+  text-decoration: line-through;
+}
+
 .text {
   flex: 1;
   display: flex;
